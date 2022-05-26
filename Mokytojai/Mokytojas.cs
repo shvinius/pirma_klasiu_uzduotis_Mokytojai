48db4d1d3946b54df14824d0d4f82efb17a0d006
shvinius <shvinius@inbox.lt>
2022-05-26 18:22:30 +0300

    susikuriau kintamuosius ir konstruktorius

---
+++ Mokytojai/Mokytojas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MokytojoKlasesObjektas
+{
+    internal class Mokytojas
+    {
+        string vardas;
+        string pavarde;
+        int klase;
+        string pareigos;
+        int stazas;
+        string issilavinimas;
+
+        public Mokytojas(string vardas, string pavarde, int klase, string pareigos, int stazas, string issilavinimas)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.klase = klase;
+            this.pareigos = pareigos;
+            this.stazas = stazas;
+            this.issilavinimas = issilavinimas;
+        }
+    }
+}
